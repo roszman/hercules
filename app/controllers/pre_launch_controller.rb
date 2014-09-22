5c18f1d71cd4ec43378c9bfc0cb67c6492bf468f
roszman <roszman@gmail.com>
2014-09-22 20:54:04 +0200

    add PreLaunch index mock

---
+++ app/controllers/pre_launch_controller.rb
@@ -0,0 +1,9 @@
+class PreLaunchController < ApplicationController
+  layout  "blank_layout"
+  def index
+  end
+
+  def index2
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pre_launch/' => 'pre_launch#index'
+
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
