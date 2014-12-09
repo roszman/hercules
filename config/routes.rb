1263228fbf61b5be073b78b0234d435d89bd080f
roszman <roszman@gmail.com>
2014-12-09 17:14:08 +0100

    site root is pointing prelauch#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pre_launch', to: 'pre_launch#index'
   post 'pre_launch/subscribe', to: 'pre_launch#subscribe'
-  root to: "home#index"
+  root to: "pre_launch#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
